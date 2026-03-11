fd0bac02d5f90e4284f1a241bea6d8a1fa0aa55c
Julian Suszko <s33103@pjwstk.edu.pl>
2026-03-11 21:03:05 +0100

    Added StatisticsHelper class and vector distance calculating function within it

---
+++ src/StatisticsHelper.java
@@ -0,0 +1,9 @@
+public class StatisticsHelper {
+    public static double calculateDistance(double x[], double y[]){
+        int sum = 0;
+        for (int i = 0; i < x.length; i++) {
+            sum += Math.pow(x[i] - y[i], 2);
+        }
+        return Math.sqrt(sum);
+    }
+}
