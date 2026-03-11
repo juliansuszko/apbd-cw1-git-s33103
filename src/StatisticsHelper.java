21e02e14b056381305fab03b36c5381412d0f481
Julian Suszko <s33103@pjwstk.edu.pl>
2026-03-11 21:11:11 +0100

    Added error handling when x and y vectors are different in calculateDistance func

--- src/StatisticsHelper.java
+++ src/StatisticsHelper.java
@@ -1,5 +1,10 @@
 public class StatisticsHelper {
     public static double calculateDistance(double x[], double y[]) {
+        if (x.length != y.length) {
+            throw new IllegalArgumentException("Arrays must be of the same length");
+        }
+
+
         double sum = 0;
         for (int i = 0; i < x.length; i++) {
             sum += Math.pow(x[i] - y[i], 2);
