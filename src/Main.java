01cb288663fb0186e4a43a36e66e3da1042f2be1
Julian Suszko <s33103@pjwstk.edu.pl>
2026-03-11 21:15:31 +0100

    Added the ability for the user to enter his own vectors and calculate distance.

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,30 @@ public class Main {
         System.out.print("Enter your name: ");
         String name = sc.nextLine();
 
-        System.out.println("Hello " + name);
+        double[] x = new double[4];
+        double[] y = new double[4];
+
+        System.out.print("Enter x vector numbers (max 4):");
+        for (int i = 0; i < 4; i++) {
+            x[i] = sc.nextDouble();
+        }
+
+        System.out.print("Enter y vector numbers (max 4):");
+        for (int i = 0; i < 4; i++) {
+            y[i] = sc.nextDouble();
+        }
+        System.out.println("Hello " + name + "!" + " Here are the vectors you entered:");
+        System.out.print("x: ");
+        for (double num : x) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        System.out.print("y: ");
+        for (double num : y) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        System.out.println("Distance: " + StatisticsHelper.calculateDistance(x, y));
+        sc.close();
     }
 }
